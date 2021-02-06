9828bc402be2924d6bb1ff71ca1675179035f989
Michael Hadley <michaelwesthadley@gmail.com>
2021-02-06 16:56:52 -0600

    Add week 03 starter files

---
+++ Week 03/react-quiz-game-starter-files/src/app.js
@@ -0,0 +1,5 @@
+function App() {
+  return <h1>Starter Files</h1>;
+}
+
+export default App;
---
+++ Week 03/react-quiz-game-starter-files/src/index.js
@@ -0,0 +1,16 @@
+/*
+  These starter files correspond to video 8: https://youtu.be/DHYiXjbUjuM. This includes the CSS &
+  image files that we use in the video, along with placeholder JS files. If you download and use
+  these, remember to run "npm install" first.
+ */
+
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./app";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.querySelector("#root")
+);
